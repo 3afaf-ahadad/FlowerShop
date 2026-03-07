62630ed418a849af58a29dfdd5e07a52e6aa512c
Afaf <afaf20ahadad@gmail.com>
2026-03-07 23:10:07 +0000

    refactor: updated cart management logic and routes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\CartController;
 
 Route::get('/', [ProductController::class, 'index']);
 
-// Afficher le panier
+
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 
-// Action d'ajouter (POST)
+
 Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 
-// Action de supprimer (DELETE)
+
 Route::delete('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
