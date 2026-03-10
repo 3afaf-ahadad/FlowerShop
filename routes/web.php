d8107e623d2306f6ec8da4d25599def1e2e6b95a
soukaina asg <soukainaasgadrane@gmail.com>
2026-03-10 19:45:44 +0000

    fix merge conflict web.php

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,23 @@
 <?php
 
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CheckoutController;
-<<<<<<< HEAD
 use App\Http\Controllers\AdminController;
-=======
->>>>>>> 71ae36b44f0bb1abab62d073365f2a909512db1d
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
-
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
+
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 Route::delete('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
 Route::patch('/cart/update', [CartController::class, 'update'])->name('cart.update');
+
 Route::get('/product/{slug}', [ProductController::class, 'show'])->name('products.show');
+
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
 
@@ -41,26 +39,13 @@ Route::get('/confirmation', function(){
 
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
 
-    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/orders', [App\Http\Controllers\OrderController::class, 'adminIndex'])->name('orders.index');
+    Route::get('/orders', [OrderController::class, 'adminIndex'])->name('orders.index');
 
-    Route::post('/orders/{id}/ship', [App\Http\Controllers\OrderController::class, 'ship'])->name('orders.ship');
+    Route::post('/orders/{id}/ship', [OrderController::class, 'ship'])->name('orders.ship');
 
     Route::resource('products', App\Http\Controllers\Admin\ProductController::class);
 
     Route::resource('categories', App\Http\Controllers\Admin\CategoryController::class);
 });
-
-<<<<<<< HEAD
-Route::prefix('admin')->middleware(['auth'])->group(function () {
-Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
-    
-});
-=======
-
-
-
-
-
->>>>>>> 71ae36b44f0bb1abab62d073365f2a909512db1d
